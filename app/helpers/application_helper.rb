f203081eba10939e100ef9bdd87ad19d25b0c004
Baptiste Pichot <baptiste.pichot@mdal.fr>
2014-03-26 16:49:06 +0100

    layouts intro

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Return a title on a per-page basis
+  # (pour prendre en compte le cas où un titre n'est pas défini pr la page)
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
